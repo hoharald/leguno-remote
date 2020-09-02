cb87a7425fd2468d1397dd6874e43d2f1414eff6
hoharald <harald.holzner@outlook.com>
2020-09-02 07:57:54 +0200

    Update TechnicHub.h

--- sketch/TechnicHub.h
+++ sketch/TechnicHub.h
@@ -1,3 +1,21 @@
+/*  This library allows to create and convert received packages
+    from a LEGO Technic Hub with integrated BLE
+    Copyright (C) 2020  Harald Holzner
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+    
 #ifndef _TECHNIC_HUB_H_
 #define _TECHNIC_HUB_H_
 
